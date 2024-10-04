655d44fc6f155613db98d1cead69baf7789d8aea
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-04 03:47:14 -0600

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 
 export default {
-  development: {
     client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
@@ -10,17 +9,6 @@ export default {
       database: process.env.DB_DBNAME,
       charset: "utf8",
     },
-  },
-  production: {
-    client: "mysql2",
-    connection: process.env.JAWSDB_URL || {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      charset: "utf8",
-    },
-  },
 };
 
 
