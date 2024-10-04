53c2d7be3f3862e70895370b34e1e2333c87aa81
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-04 02:56:58 -0600

    update jnexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,19 +5,9 @@ export default {
     client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
-      user: process.env.DB_LOCAL_USER,
-      password: process.env.DB_LOCAL_PASSWORD,
-      database: process.env.DB_LOCAL_DBNAME,
-      charset: "utf8",
-    },
-  },
-  production: {
-    client: "mysql2",
-    connection: process.env.JAWSDB_URL || {
-      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_DBNAME,
       charset: "utf8",
     },
   },
