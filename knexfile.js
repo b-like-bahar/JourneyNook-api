120681fe4a0722c6455b50b5b8665be2f1623d2a
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-04 03:11:25 -0600

    change knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -11,16 +11,6 @@ export default {
       charset: "utf8",
     },
   },
-  production: {
-    client: "mysql2",
-    connection: process.env.JAWSDB_URL || {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      charset: "utf8",
-    },
-  },
 };
 
 
