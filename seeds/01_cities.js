38fde51b71fdd9aa95ad54a1b4089a5876efeae3
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-04 16:17:19 -0600

    change the path of images in cities seed file to a cloud url

--- seeds/01_cities.js
+++ seeds/01_cities.js
@@ -10,61 +10,61 @@ export async function seed(knex) {
       id: 1,
       city_name: 'Cancun',
       country: 'Mexico',
-      city_image_path: '/images/cancun/cancun-mexico.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039156/Cancun-Mexico_dr37di.png'
     },
     {
       id: 2,
       city_name: 'Barcelona',
       country: 'Spain',
-      city_image_path: '/images/barcelona/barcelona-spain.png'
+      city_image_path: '/https://res.cloudinary.com/deuku9jpm/image/upload/v1728039155/Barcelona-Spain_zswk2w.png'
     },
     {
       id: 3,
       city_name: 'Istanbul',
       country: 'Turkey',
-      city_image_path: '/images/istanbul/istanbul-turkey.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039161/Istanbul-Turkey_suqpnd.png'
     },
     {
       id: 4,
       city_name: 'London',
       country: 'England',
-      city_image_path: '/images/london/london-england.png'
+      city_image_path: '/https://res.cloudinary.com/deuku9jpm/image/upload/v1728039163/London-England_v4ihhe.png'
     },
     {
       id: 5,
       city_name: 'Moscow',
       country: 'Russia',
-      city_image_path: '/images/moscow/moscow-russia.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039169/Moscow-Russia_fijh6n.png'
     },
     {
       id: 6,
       city_name: 'Paris',
       country: 'France',
-      city_image_path: '/images/paris/paris-france.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039170/Paris-France_iyo7f7.png'
     },
     {
       id: 7,
       city_name: 'Rio de Janeiro',
       country: 'Brazil',
-      city_image_path: '/images/rio-de-janeiro/rio-de-janeiro-brazil.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039175/rio-de-janeiro-brazil_yhmdgw.png'
     },
     {
       id: 8,
       city_name: 'Rome',
       country: 'Italy',
-      city_image_path: '/images/rome/rome-italy.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039177/Rome-Italy_izcysv.png'
     },
     {
       id: 9,
       city_name: 'Shanghai',
       country: 'China',
-      city_image_path: '/images/shanghai/shanghai-china.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039182/Shanghai-China_tqpppd.png'
     },
     {
       id: 10,
       city_name: 'Sydney',
       country: 'Australia',
-      city_image_path: '/images/sydney/sydney-australia.png'
+      city_image_path: 'https://res.cloudinary.com/deuku9jpm/image/upload/v1728039182/Sydney-Australia_jvfwxc.png'
     }
   ]
 );
