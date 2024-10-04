94262aa97f4ca23b25b02eea639c13fc45e9a4c7
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-04 00:28:41 -0600

    update knexfile to use jawsdb url when needed

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,20 @@
 import "dotenv/config";
 
 export default {
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-    database: process.env.DB_LOCAL_DBNAME,
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_DBNAME,
+      charset: "utf8",
+    },
+  },
+  production: {
+    client: "mysql2",
+    connection: process.env.JAWSDB_URL,
     charset: "utf8",
   },
 };
+
